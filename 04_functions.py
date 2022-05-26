8a3ee424d36db2d94d92e17347275681f0737ed1
lilWeagant <rdweagant@outlook.com>
2022-05-26 10:31:45 -0400

    added examples to 05_functions_2.py

--- 04_functions.py
+++ 04_functions.py
@@ -1,26 +1,26 @@
-#Function with no arguments or return value
+# #Function with no arguments or return value
 
-def say_hello():
-    print("Hello!")
+# def say_hello():
+#     print("Hello!")
 
-say_hello()
+# say_hello()
 
-#Function with return value (but no arguments)
+# #Function with return value (but no arguments)
 
-def get_answer():
-    return 42
+# def get_answer():
+#     return 42
 
-# print the value directly
-print(get_answer())
-# assign to a variable
-answer  = get_answer()
-print(answer)
+# # print the value directly
+# print(get_answer())
+# # assign to a variable
+# answer  = get_answer()
+# print(answer)
 
-# Function with return value and arguments
-def times_two(x):
-    answer = x*2
-    return answer
-#print(number) #number is local to times_two function
-print(times_two(4)) # print 8
-print(times_two(7)) # print 14
-print(answer) # print 14, or 42?
+# # Function with return value and arguments
+# def times_two(x):
+#     answer = x*2
+#     return answer
+# #print(number) #number is local to times_two function
+# print(times_two(4)) # print 8
+# print(times_two(7)) # print 14
+# print(answer) # print 14, or 42? -> 42
---
+++ 05_functions_2.py
@@ -0,0 +1,59 @@
+# # Review of arguments and return values
+# def print_message(message):
+#     print(message)
+
+# def add_to_message(message):
+#     message += ' - From Riley' # += -> message = message + ' - From Riley'
+#     return message
+
+# mes = 'hello CSCI1030!'
+# print_message(mes)
+# print(add_to_message(mes))
+# message = add_to_message(mes)
+# print(message)
+# print(mes)
+
+# def times_two(x):
+#     x = x*2
+
+# x = 4
+# print(x) # print 4
+# times_two(x) # function call
+# print(x) # what value will be printed to the console? -> 4 because of local variable and pass by value
+
+# def add_to_list(list):
+#     list.append(3)
+#     return list
+
+# list1 = [1, 2, 3]
+# print(list1)
+# list1 = add_to_list(list1)
+# print(list1) # what value will be printed to the console? -> [1, 2, 3, 3] because pass by reference
+
+# # Demonstrate Calling Stack -> do this at beginning on Tuesday May 31st
+
+# def func1(a, b):
+#     x = 1
+#     return a + b + x
+
+# def func2(x):
+#     y = 8
+#     z = func1(x, y)
+#     return z
+
+# print(func2(4))
+
+# Coding Exercise
+
+x = 12345 # 12345/10 = 12340 with remainder of 5
+
+def to_str(num):
+    result = ''
+    while num > 0:
+        # % -> modulus operator
+        digit = num % 10
+        num = num//10 # floor division -> truncate decimal point
+        result += str(digit) # += -> shorthand for result = result + str(digit)
+    return result
+
+print(to_str(12345))
