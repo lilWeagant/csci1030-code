2e21f5f076de629a6e4414a1fb754894caec87e0
lilWeagant <rdweagant@outlook.com>
2022-06-02 15:45:19 -0400

    added regex example

--- 07_higher_order_functions.py
+++ 07_higher_order_functions.py
@@ -1,8 +1,46 @@
-def traverse(elements, op):
-    for element in elements:
-        op(element)
+# def traverse(elements, op):
+#     for element in elements:
+#         op(element)
 
-def output(x):
+# def output(x):
+#     print(x)
+
+# traverse([1, 2, 3], output)
+
+# # Lambda expression is essentially an unnamed function
+# add = lambda x,y: x + y 
+# z = add(1, 2)
+# print(z)
+
+# # 'map' Function -> e.g. converting fahrenheit to celsius
+# def f_to_c(f):
+#     return (f - 32) * 5 / 9
+
+# f_temps = [60.0, 70.0, 80.0, 90.0, 100.0]
+# c_temps = map(f_to_c, f_temps)
+# for temp in c_temps:
+#     print(temp)
+
+# # 'reduce' Function -> e.g. get the sum of elements in a list
+# from functools import reduce
+
+# def add(x, y):
+#     return x + y
+# sum = reduce(add, [1, 2, 3, 4, 5])
+
+# sum2 = reduce(lambda x,y: x + y, [1, 2, 3, 4, 5])
+# print(sum)
+# print(sum2)
+
+# 'filter' Function -> e.g. filter grades > 80
+grades = [64.5, 87.0, 94.0, 71.5, 46.0, 100.0]
+
+def a_filter(x):
+    return x > 80.0
+
+# a_grades = filter(a_filter, grades)
+# for x in a_grades:
+#     print(x)
+a_grades2 = filter(lambda x: x > 80.0, grades)
+for x in a_grades2:
     print(x)
-
-traverse([1, 2, 3], output)
--- 08_regex.py
+++ 08_regex.py
@@ -1,2 +1,26 @@
+# [a-zA-Z_]\w*
+# A letter or underscore, followed by 0 or more letters, digits, or underscores
+# a_variable_name -> match
+# v -> match
+# 9_variable -> would not match -> starts with a digit
 
+# (1-)?[0-9]{3}-[0-9]{3}-[0-9]{4}
+# A north american phone number with optional 1-prefix
+# 1-905-721-8668 -> match
+# 905-721-8668 -> match
+# 9057218668 -> would not match -> needs dashes between number sets 
+
+import re # imports regular expression package for Python
+
+#define a regex -> recognize a name -> capital letter followed by 0 or more lowercase letters
+# [A-Z][a-z]*
+nameRE = re.compile('[A-Z][a-z]*')
+match = nameRE.match('Walt Breslin')
+print(match)
+if match: #will execute if the string is a match
+    print('Start: ', match.start())
+    print('End: ', match.end())
+    print('Text: ', match.group())
+print('All names: ', nameRE.findall('Walt Breslin'))
+print('All names: ', nameRE.findall('Joaquin El Chapo Guzman'))
 
